55a1cd6251735af5b4e562ae2b71a4586177334b
likhitha.ns <likhitha.ns@collabera.com>
2022-01-19 14:18:54 +0530

    cicd-jenkins-demo explainations and pushing dockerfile once again doing procedure

---
+++ src/test/java/com/ani/cicd/cicdjenkinsdemo/MobileTests.java
@@ -0,0 +1,15 @@
+package com.ani.cicd.cicdjenkinsdemo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MobileTests {
+
+
+    @DisplayName("checking mobile class")
+    @Test
+    public void testMobile()
+    {
+        System.out.println("tested properly");
+    }
+}
