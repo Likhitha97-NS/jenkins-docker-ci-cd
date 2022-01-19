baef9e5993b846f32fe2639411acc933246f5723
likhitha.ns <likhitha.ns@collabera.com>
2022-01-19 10:58:31 +0530

    cicd-jenkins-demo explaination

--- src/main/java/com/ani/cicd/cicdjenkinsdemo/CicdJenkinsDemoApplication.java
+++ src/main/java/com/ani/cicd/cicdjenkinsdemo/CicdJenkinsDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CicdJenkinsDemoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CicdJenkinsDemoApplication.class, args);
 	}
