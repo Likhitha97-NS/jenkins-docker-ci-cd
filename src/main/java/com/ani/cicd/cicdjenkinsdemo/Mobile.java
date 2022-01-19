61c9ec8c32f1e0629b9e90c750a660239cfb7949
likhitha.ns <likhitha.ns@collabera.com>
2022-01-19 13:45:11 +0530

    cicd-jenkins-demo explainations and pushing dockerfile

--- src/main/java/com/ani/cicd/cicdjenkinsdemo/Mobile.java
+++ src/main/java/com/ani/cicd/cicdjenkinsdemo/Mobile.java
@@ -1,4 +1,13 @@
 package com.ani.cicd.cicdjenkinsdemo;
 
 public class Mobile {
+    private int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
